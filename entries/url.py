41c64527ef7606d8b2dcbf9417518cf08aad2fa5
caamartinezbu <carlosmartinez622@gmail.com>
2023-08-22 23:26:36 -0500

    se creo clases de crud y inicio de session geneal con el admin de django creando una clase general

--- entries/url.py
+++ entries/url.py
@@ -4,11 +4,23 @@ from . import views
 
 urlpatterns = [
     path("", views.EntryListView.as_view(),
-        name= " entry-list"
+            name= "entry-list"
     ),
 
-    path("detalle/<int:pk>", views.EntryDetailView.as_view(),
-        name = "entry-detail"
+    path("entry/<int:pk>", views.EntryDetailView.as_view(),
+            name = "entry-detail"
+    ),
+
+    path("create", views.EntryCreateView.as_view(),
+            name = "entry-create"
+    ),
+
+    path("entry/<int:pk>/update", views.EntryUpdateView.as_view(),
+            name = "entry-update",
+    ),
+
+    path("entry/<int:pk>/delete",views.EntryDeleteView.as_view(),
+            name = "entry-delete",
     ),
 
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,14 +1,50 @@
-from django.shortcuts import render
-from django.views.generic import (ListView, DetailView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Entry
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+
 # Create your views here.
 
 
-class EntryListView(ListView):
+class LockedView(LoginRequiredMixin):
+    login_url = 'admin:login'
+
+class EntryListView(LockedView, ListView):
     model = Entry
     # devolverá sus entradas en orden ascendente, con la entrada más reciente en la parte superior de la lista.
     queryset = Entry.objects.all().order_by("-data_created")
 
-class EntryDetailView(DetailView):
+class EntryDetailView(LockedView, DetailView):
     model = Entry
+
+
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "¡Tu nueva publicación fue creada!"
+
+
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = " ¡Tu publicación fue actualizada!"
+
+    def get_success_url(self):
+      
+        return reverse_lazy(
+            "entry-detail",
+            kwargs = { "pk": self.object.pk }
+        )
+    
+class EntryDeleteView(LockedView, DeleteView):
+    model = Entry
+    success_url  = reverse_lazy("entry-list")
+    success_message = "¡Tu publicación fue eliminada!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
 
